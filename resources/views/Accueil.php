623d2b5653c7a763035e41fb50c8c5aa50712230
glorpnork <elioharchi74@gmail.com>
2026-09-23 16:17:44 +0200

    je sais pas encore je fais que de bouffer de la docs

---
+++ resources/views/Accueil.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appli météo </title>
+    <link rel="stylesheet" href="resources/css/app.css">
+</head>
+<body>
+    <h1> Application météo</h1>
+    <p>L'appli météo est en cours de construction</p>
+    <p id="container-meteo"></p>
+    
+</body>
+</html>
